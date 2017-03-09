ad97cd73919df5dafd7358453ed03030e2debf53
quincy-baker <m4tth3w.3l1a@gmail.com>
2017-03-09 01:05:41 -0500

    checkoutToNewBranchForLaterReferenceIfNecessary

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the byndApp
  */
 angular.module('byndApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
