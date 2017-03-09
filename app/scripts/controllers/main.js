57ee084f8f57ea1831e37828a699ffa8e7d69644
quincy-baker <m4tth3w.3l1a@gmail.com>
2017-03-09 09:06:00 -0500

    --allow-empty

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,34 +9,34 @@
  */
 
 angular.module('byndApp')
-  .controller('MainCtrl', function($scope, $http) {
+  .controller('MainCtrl', function($http, $scope) {
     $http({
-      method : "GET",
-      url : "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw"
+      method : 'GET',
+      url : 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw'
     }).then(function success(response) {
       
       console.log(response);
 
-      // var i = 0;
-      // var el_itemDescription = $('#description-wrapper');
-      // var els__itemDescriptions = [];
-      // var video_items_array_size = [];
-     // ============================================================================ //
+      var i = 0;
+      var el_itemDescription = $('#description-wrapper');
+      var els__itemDescriptions = [];
+      var video_items_array_size = [];
+    // ============================================================================ //
 
       // note alias: YT => youtube
-      // var YT_videoItems= response.data.items;
+      var YT_videoItems= response.data.items;
       
-      // // $scope.playlist = response.data.items;
+      $scope.playlist = response.data.items;
       // $scope.videoId = response.data.contentDetails.videoId;     
 
-      // $scope.title = YT_videoItems.title;
-      // $scope.description = YT_videoItems.description;
+      $scope.title = YT_videoItems.title;
+      $scope.description = YT_videoItems.description;
 
-      // $.each($el_itemDescription, function (key,val) {
-      //   console.log(key,val);
-        // console.log(video_item_description_char_length);
+      $.each(el_itemDescription, function (key,val) {
+        console.log(key,val);
+        console.log(video_item_description_char_length);
         // videoItemDescriptions.push()
-      // });
+      });
 
       // for (i=0; i < videoItemDescriptions.length - 1; i++) {
       //   videoItemDescriptions[i]
