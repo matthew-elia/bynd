b302d862a5d625a50f79143254343bd754e457f7
quincy-baker <m4tth3w.3l1a@gmail.com>
2017-03-09 20:56:48 -0500

    responsive desktop2mobile // youtube FEED integration // pagination

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,41 +12,45 @@ angular.module('byndApp')
   .controller('MainCtrl', function($http, $scope) {
     $http({
       method : 'GET',
-      url : 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw'
+      url : 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=38&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw'
     }).then(function success(response) {
       
       console.log(response);
-
       var i = 0;
-      var el_itemDescription = $('#description-wrapper');
-      var els__itemDescriptions = [];
-      var video_items_array_size = [];
-    // ============================================================================ //
+      var payload = response.data;
 
-      // note alias: YT => youtube
-      var YT_videoItems= response.data.items;
-      
-      $scope.playlist = response.data.items;
-      // $scope.videoId = response.data.contentDetails.videoId;     
+      var totalResults = payload.pageInfo.totalResults;
+      var resultsPerPage = payload.pageInfo.resultsPerPage;
 
-      $scope.title = YT_videoItems.title;
-      $scope.description = YT_videoItems.description;
+      $scope.currentPage = 0;
+      $scope.pageSize = 1;
+      $scope.data = [];
 
-      $.each(el_itemDescription, function (key,val) {
-        console.log(key,val);
-        console.log(video_item_description_char_length);
-        // videoItemDescriptions.push()
-      });
+      $scope.numberOfPages = function() {
+          return Math.ceil($scope.data.length/$scope.pageSize);                
+      }
 
-      // for (i=0; i < videoItemDescriptions.length - 1; i++) {
-      //   videoItemDescriptions[i]
-      // }
-      // if (n%20 == 0 && n > 0) {
-      //   $('#description-wrapper').append('<br>');
-      // }
+      for (var idx=0; idx<totalResults; idx++) {
+          $scope.data.push("item "+idx);
+      }
+
+
+      $scope.videoItems = payload.items;
+      $scope.title = payload.items.title;
+      $scope.description = payload.items.description;
+
 
     }).then(function error(response) { 
       console.log(response);
-      // $scope.errorResponse = response.statusText;
+      $scope.errorResponse = response.statusText;
     });
+
+}).filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
 });
+
+//We already have a limitTo filter built-in to angular,
+//let's make a startFrom filter
---
+++ app/scripts/services/pagination.js
@@ -0,0 +1,8 @@
+// var app=angular.module('byndApp', []);
+
+// alternate - https://github.com/michaelbromley/angularUtils/tree/master/src/directives/pagination
+// alternate - http://fdietz.github.io/recipes-with-angular-js/common-user-interface-patterns/paginating-through-client-side-data.html
+
+// app.service('Pagination', ['$scope', '$filter', function ($scope, $filter) {
+
+// }]);
